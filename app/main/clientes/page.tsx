176502cc56dca3bbe05bed45abc0bbab4cf75f85
Gustavo Esmanhotto Bareta <gustavo.bareta@mps.interno>
2025-04-16 17:41:05 -0300

    Client Page

--- app/main/clientes/page.tsx
+++ app/main/clientes/page.tsx
@@ -5,24 +5,6 @@ import Button from "@/components/UI/Button";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
-type Row = {
-    id: number;
-    nome: string;
-    email: string;
-    telefone: string;
-};
-
-const columns = [
-    { id: "nome", column: "Nome" },
-    { id: "email", column: "Email" },
-    { id: "telefone", column: "Telefone" },
-];
-
-const data: Row[] = [
-    { id: 1, nome: "João Silva", email: "joao@email.com", telefone: "(41)99717-3484" },
-    { id: 2, nome: "Maria Souza", email: "maria@email.com", telefone: "(41)99874-2498" },
-    { id: 3, nome: "Carlos Lima", email: "carlos@email.com", telefone: "(41)3364-7375" },
-];
 const queryClient = new QueryClient();
 export default function TabelaEstilizada() {
     return (
@@ -30,7 +12,7 @@ export default function TabelaEstilizada() {
             <div className="flex justify-between items-center mb-4 shadow-md">
                 <h2 className="text-xl font-bold">Tabela de Usuários</h2>
                 <div className="mb-2">
-                    <Button>Pesquisa Avançada</Button>
+                    <Button>Configurações</Button>
                 </div>
             </div>
             <QueryClientProvider client={queryClient}>
